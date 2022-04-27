21a4e52e8b5621eb22309730b0a1c4f5b8d158c8
damlayagmur <dmlygmr@windowslive.com>
2022-04-27 23:00:13 +0300

    suspend fun

--- app/src/main/java/com/damlayagmur/coroutineexample/MainActivity.kt
+++ app/src/main/java/com/damlayagmur/coroutineexample/MainActivity.kt
@@ -15,10 +15,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         GlobalScope.launch {
-            delay(5000L)
-            Log.d(TAG, "Coroutines says hello from thread ${Thread.currentThread().name}")
+            val networkCallAnswer = doNetworkCall()
+            val networkCallAnswer2 = doNetworkCall2()
+            Log.d(TAG, networkCallAnswer)
+            Log.d(TAG, networkCallAnswer2)
+
+            //delay(5000L)
+            //Log.d(TAG, "Coroutines says hello from thread ${Thread.currentThread().name}")
         }
         Log.d(TAG, "Hello from thread ${Thread.currentThread().name}")
+    }
+
+    suspend fun doNetworkCall(): String {
+        delay(3000L)
+        return "This is the answer"
+    }
+
+    suspend fun doNetworkCall2(): String {
+        delay(3000L)
+        return "This is the answer2"
     }
 }
