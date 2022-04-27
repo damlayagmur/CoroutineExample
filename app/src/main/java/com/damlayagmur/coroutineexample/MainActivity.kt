c669f4117fabfe1d35a922104d3ec305c5cc15d3
damlayagmur <dmlygmr@windowslive.com>
2022-04-27 22:47:54 +0300

    launch and delay

--- app/src/main/java/com/damlayagmur/coroutineexample/MainActivity.kt
+++ app/src/main/java/com/damlayagmur/coroutineexample/MainActivity.kt
@@ -2,10 +2,23 @@ package com.damlayagmur.coroutineexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        GlobalScope.launch {
+            delay(5000L)
+            Log.d(TAG, "Coroutines says hello from thread ${Thread.currentThread().name}")
+        }
+        Log.d(TAG, "Hello from thread ${Thread.currentThread().name}")
     }
 }
