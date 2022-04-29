4b4cb70199f5cf405702cba6964d0a7b96820824
damlayagmur <dmlygmr@windowslive.com>
2022-04-29 21:29:31 +0300

    Coroutines Contexts

--- app/src/main/java/com/damlayagmur/coroutineexample/MainActivity.kt
+++ app/src/main/java/com/damlayagmur/coroutineexample/MainActivity.kt
@@ -3,17 +3,19 @@ package com.damlayagmur.coroutineexample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import android.widget.TextView
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
 
     val TAG = "MainActivity"
+    lateinit var tvDummy: TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tvDummy = findViewById(R.id.tvDummy)
 
 
         GlobalScope.launch {
@@ -25,15 +27,24 @@ class MainActivity : AppCompatActivity() {
             //delay(5000L)
             //Log.d(TAG, "Coroutines says hello from thread ${Thread.currentThread().name}")
         }
+
+        GlobalScope.launch(Dispatchers.IO) {
+            Log.d(TAG,"Starting coroutine in thread ${Thread.currentThread().name}")
+            val answer = doNetworkCall()
+            withContext(Dispatchers.Main) {
+                Log.d(TAG,"Setting text in thread ${Thread.currentThread().name}")
+                tvDummy.text = answer
+            }
+        }
         Log.d(TAG, "Hello from thread ${Thread.currentThread().name}")
     }
 
-    suspend fun doNetworkCall(): String {
+    private suspend fun doNetworkCall(): String {
         delay(3000L)
         return "This is the answer"
     }
 
-    suspend fun doNetworkCall2(): String {
+    private suspend fun doNetworkCall2(): String {
         delay(3000L)
         return "This is the answer2"
     }
